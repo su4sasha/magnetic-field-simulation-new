795523b5ed7b20b9a261ab3d07ee18f91a9e7891
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-03-14 18:55:59 +0300

    Update particles.py

--- particles.py
+++ particles.py
@@ -1,9 +1,9 @@
 class Particle:
-    def __init__(self, coords: list[int], speed: float, speed_direction: str, charge_sign: str, charge_value: float, mass: float, is_seen_on_screen: bool):
+    def __init__(self, coords: list[int], speed: float, speed_direction: float, charge_sign: str, charge_value: float, mass: float, is_seen_on_screen: bool):
         self.particle_data = {
             "coords": coords,
             "speed": speed,
-            "speed_direction": speed_direction,
+            "speed_direction": speed_direction,  # angle in radians from the positive x-axis to the particle's direction
             "charge_sign": charge_sign,
             "charge_value": charge_value,
             "mass": mass,
